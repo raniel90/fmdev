1e1892420ae33fede1561b562c16c07e83d67713
Raniel Gomes da Silva <raniel90@gmail.com>
2019-03-03 13:00:50 -0300

    Init Project

---
+++ server/app.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from flask_restful import Api
+from resources.Hello import Hello
+from resources.Category import CategoryResource
+from resources.Comment import CommentResource
+
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+# Routes
+
+api.add_resource(Hello, '/hello')
+api.add_resource(CategoryResource, '/category')
+api.add_resource(CommentResource, '/comment')
